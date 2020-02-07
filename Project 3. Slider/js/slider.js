49585d774e681aafe1dc517d58d95f091804ad38
Oleg900 <Oleg90s@mail.ru>
2020-02-07 12:02:03 +0300

    Add files via upload

--- Project 3. Slider/js/slider.js
+++ Project 3. Slider/js/slider.js
@@ -1,24 +1,24 @@
 (function slider(){
     // Делает активным кнопку/ссылку слайдера
-    function circleActive() {
+    function circleActive(slider, slide) {
         let left = parseInt(slider.style.left);
         
-        newClass(sliderNavigation.children);
-        newClass(sliderNavigationAncor.children);
+        newClass(document.querySelector(".projects_slider-buttons").children);
+        newClass(document.querySelector(".projects_slider-list").children);
         
-        function newClass(mas) {
-            for (el of mas) el.classList.remove("active");
+        function newClass(arr) {
+            for (el of arr) el.classList.remove("active");
         
-            for (let i = 0; i < mas.length; i++) {
+            for (let i = 0; i < arr.length; i++) {
                 if (left === (-slide[0].offsetWidth * i)) {
-                    mas[i].classList.add("active");
+                    arr[i].classList.add("active");
                 }
             }
         }
     }
 
     // Стрелка слайдера "назад"
-    function sliderBack() {
+    function sliderBack(slider, slide) {
         slider.style.transition = "left .5s";
         let left = parseInt(slider.style.left);
         let width = slide[0].offsetWidth;
@@ -27,11 +27,11 @@
             slider.style.left = -width * (slide.length - 1) + "px";
         } else slider.style.left = left + width + "px";
 
-        circleActive();
+        circleActive(slider, slide);
     }
 
     // Стрелка слайдера "вперёд"
-    function sliderNext() {
+    function sliderNext(slider, slide) {
         slider.style.transition = "left .5s";
         let left = parseInt(slider.style.left);
         let width = slide[0].offsetWidth;
@@ -40,49 +40,58 @@
         else if ((left === -width * (slide.length - 1))) slider.style.left = 0 + "px";
         else slider.style.left = left + -width + "px";
         
-        circleActive();
+        circleActive(slider, slide);
     }
 
     // Переключение слайдера по кнопкам/ссылкам
-    function sliderOnElement() {
+    function sliderOnElement(slider, slide) {
         slider.style.transition = "left .5s";
-        sliderOnElementHelp(sliderNavigation.children, this);
-        sliderOnElementHelp(sliderNavigationAncor.children, this);
+        sliderOnElementHelp(document.querySelector(".projects_slider-list").children, event.target);
+        sliderOnElementHelp(document.querySelector(".projects_slider-buttons").children, event.target);
         
-        function sliderOnElementHelp(mas, el) {
-            for (let i = 0; i < mas.length; i++) {
-                if (mas[i] === el) {
+        function sliderOnElementHelp(arr, el) {
+            for (let i = 0; i < arr.length; i++) {
+                if (arr[i] === el) {
                     slider.style.left = -slide[0].offsetWidth * i + "px";
                 }
             }
         }
 
-        circleActive();
+        circleActive(slider, slide);
     }
 
     // Изменяет ширину слайдов и позиционирование элемента "slider"(класс "projects-slider"), 
     // делая слайдер адаптивным
-    function sliderAdaptive() {
+    function sliderAdaptive(slider, slide, sliderContainer, arrowBack, arrowNext) {
         let left = parseInt(slider.style.left);
 
         slide.forEach(el => el.style.width = sliderContainer.clientWidth + "px");
 
         slider.style.transition = "none";
         
-        let mas = sliderNavigation.children;
-        for (let i = 0; i < mas.length; i++) {
-            if (mas[i].className.includes("active")) {
+        let arr = document.querySelector(".projects_slider-buttons").children;
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i].className.includes("active")) {
                 slider.style.left = -slide[0].clientWidth * i + "px";
             }
         }
 
         if (document.body.clientWidth <= 923) {
-            let sl = document.body.clientWidth <= 683 ?  
+            let sliderWidthMini = document.body.clientWidth <= 683 ?  
                 document.querySelector(".projects_slider-images--media") :
                 document.querySelector(".projects_slider");
 
-            arrowBack.style.left = (document.body.clientWidth - sl.clientWidth) / 2 + "px";
-            arrowNext.style.right = (document.body.clientWidth - sl.clientWidth) / 2 + "px";
+            arrowBack.style.left = (document.body.clientWidth - sliderWidthMini.clientWidth) / 2 + "px";
+            arrowNext.style.right = (document.body.clientWidth - sliderWidthMini.clientWidth) / 2 + "px";
+
+            if (document.body.clientWidth <= 683) {
+                arrowBack.style.top = -slide[0].clientHeight / 3 + "px";
+                arrowNext.style.top = -slide[0].clientHeight / 3 + "px";
+            } else {
+                arrowBack.style.top = "";
+                arrowNext.style.top = "";
+            }
+
         }
 
     }
@@ -102,21 +111,21 @@
         if (i === 0) circle.classList.add("active");
         sliderNavigation.append(circle);
 
-        circle.addEventListener("click", sliderOnElement);
+        circle.addEventListener("click", () => sliderOnElement(slider, slide));
     }
 
     // Делаю активной первую ссылку слайдера
     sliderNavigationAncor.children[0].classList.add("active");
 
     for (i = 0; i < sliderNavigationAncor.children.length; i++) {
-        sliderNavigationAncor.children[i].addEventListener("click", sliderOnElement);
+        sliderNavigationAncor.children[i].addEventListener("click", () => sliderOnElement(slider, slide));
     }
 
-    arrowBack.addEventListener("click", sliderBack);
-    arrowNext.addEventListener("click", sliderNext);
-    window.addEventListener("resize", sliderAdaptive);
+    arrowBack.addEventListener("click", () => sliderBack(slider, slide));
+    arrowNext.addEventListener("click", () => sliderNext(slider, slide));
+    window.addEventListener("resize", () => sliderAdaptive(slider, slide, sliderContainer, arrowBack, arrowNext));
 
-    sliderAdaptive();
+    sliderAdaptive(slider, slide, sliderContainer, arrowBack, arrowNext);
 
     // Автоматическое пролистывание слайдов
     let n = setInterval(sliderNext, 5000);
